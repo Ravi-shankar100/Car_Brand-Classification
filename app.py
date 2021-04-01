4bd32af6ba54a8ef4bce5123dc9bf74c20f3c26c
Ravi-shankar100 <47877512+Ravi-shankar100@users.noreply.github.com>
2021-04-01 20:54:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 11 22:34:20 2020
-
-@author: Krish Naik
-"""
-
-from __future__ import division, print_function
-# coding=utf-8
-import sys
 import os
-import glob
 import re
 import numpy as np
 
 # Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-#from gevent.pywsgi import WSGIServer
+
 
 # Define a flask app
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
 MODEL_PATH ='model_resnet50.h5'
+
 
 # Load your trained model
 model = load_model(MODEL_PATH)
@@ -40,15 +29,13 @@ def model_predict(img_path, model):
 
     # Preprocessing the image
     x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
+   
     ## Scaling
     x=x/255
     x = np.expand_dims(x, axis=0)
    
-
-   
-
     preds = model.predict(x)
+    
     preds=np.argmax(preds, axis=1)
     if preds==0:
         preds="The Car is Audi"
@@ -76,7 +63,7 @@ def upload():
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
-            basepath, 'Datasets/Test', secure_filename(f.filename))
+            basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
         # Make prediction
